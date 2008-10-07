20a15d6af0e3ea22b879f25a995bb7b8b79b19c5
Jacob Lewallen <jlewallen@gmail.com>
2008-10-07 10:33:06 -0700

    First pass at working agent side Git support.

--- src/main/java/org/hivedb/teamcity/plugin/GitVcsOnAgent.java
+++ src/main/java/org/hivedb/teamcity/plugin/GitVcsOnAgent.java
@@ -16,18 +16,35 @@ import org.apache.log4j.Logger;
 public class GitVcsOnAgent implements CheckoutOnAgentVcsSupport {
   Logger log = Logger.getLogger(GitVcsOnAgent.class);
 
+  private static final String GIT_COMMAND = "git_command";
+  private static final String CLONE_URL = "clone_url";
+
   public GitVcsOnAgent(BuildAgentConfiguration agentConf) {
   }
 
   public void updateSources(BuildProgressLogger logger, File workingDirectory, VcsRoot root, String newVersion, IncludeRule includeRule)
    throws VcsException {
     log.info("updateSources: " + workingDirectory + " " + newVersion);
+    if (!git(root, workingDirectory.getParentFile(), workingDirectory).isGitRepo(workingDirectory.getAbsolutePath())) {
+      /* TeamCity creates this directory for us, nice of them... Git dislikes that though. */
+      workingDirectory.delete();
+      git(root, workingDirectory.getParentFile(), workingDirectory).clone(root.getProperty(CLONE_URL));
+    }
   }
 
   public String getName() {
     return "git";
   }
+
+  private String[] getGitCommand(VcsRoot vcsRoot) {
+    String path = vcsRoot.getProperty(GIT_COMMAND);
+    if (new File(path).exists()) {
+      return new String[] { path };
+    }
+    return new String[] { "/usr/bin/git" };
+  }
+
+  private Git git(VcsRoot root, File workingDirectory, File projectDirectory) {
+    return new Git(getGitCommand(root), workingDirectory.getAbsolutePath(), projectDirectory.getAbsolutePath());
+  }
 }
-
-
-
