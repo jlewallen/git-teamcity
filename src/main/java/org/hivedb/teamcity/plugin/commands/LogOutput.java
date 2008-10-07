fdd3ee14f7e63334bd3267d60269b1b68a8c11af
Jacob Lewallen <jlewallen@gmail.com>
2008-10-07 16:57:27 -0700

    Fixing log output for when there's multiple lines in a log message.

--- src/main/java/org/hivedb/teamcity/plugin/commands/LogOutput.java
+++ src/main/java/org/hivedb/teamcity/plugin/commands/LogOutput.java
@@ -24,33 +24,36 @@ public class LogOutput {
     try {
       Commit current = new Commit();
       while ((line = r.readLine()) != null) {
-        String s = line.trim();
-        if (s == null || "".equals(s))
+        if (line == null || line.trim().equals(""))
           continue;
         else {
-          if (s.startsWith("commit")) {
+          if (line.startsWith("commit")) {
             if (current.isValid()) {
               commits.add(current);
               current = new Commit();
             }
-            current.setHash(s.replaceAll("commit", "").trim());
-          } else if (s.startsWith("Author"))
-            current.setAuthor(s.replaceAll("Author:","").trim());
-          else if (s.startsWith("Date")) {
-            String dateString = s.replaceAll("Date:","").trim();
-            current.setDate(Constants.GIT_DATE.parse(dateString));
-          } else {
-            current.setMessage(s);
-            commits.add(current);
-            current = new Commit();
+            current.setHash(line.replaceAll("commit", "").trim());
           }
-          
-          Matcher m = nameAndStatus.matcher(line);
-          while (m.find()) {
-            NameAndStatus change = new NameAndStatus(m.group(2), m.group(1));
-            current.getChanges().add(change);
+          else if (line.startsWith("Author"))
+            current.setAuthor(line.replaceAll("Author:", "").trim());
+          else if (line.startsWith("Date")) {
+            String dateString = line.replaceAll("Date:", "").trim();
+            current.setDate(Constants.GIT_DATE.parse(dateString));
+          }
+          else if (line.startsWith("\\t")) {
+            current.setMessage(current.getMessage() + "\n" + line);
+          }
+          else {
+            Matcher m = nameAndStatus.matcher(line);
+            while (m.find()) {
+              NameAndStatus change = new NameAndStatus(m.group(2), m.group(1));
+              current.getChanges().add(change);
+            }
           }
         }
+      }
+      if (current.isValid()) {
+        commits.add(current);
       }
     }
     catch (IOException e) {
