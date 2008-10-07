5e8b391426530de92ed49fa1cfd2374c3bfec649
Jacob Lewallen <jlewallen@gmail.com>
2008-10-06 22:06:25 -0700

    Adding a stub for the VcsCheckoutOnAgent support.

---
+++ src/main/java/org/hivedb/teamcity/plugin/GitVcsOnAgent.java
@@ -0,0 +1,32 @@
+package org.hivedb.teamcity.plugin;
+
+import java.io.File;
+import java.util.*;
+import java.io.IOException;
+
+import jetbrains.buildServer.util.*;
+import jetbrains.buildServer.vcs.*;
+import jetbrains.buildServer.CollectChangesByIncludeRule;
+import jetbrains.buildServer.agent.vcs.CheckoutOnAgentVcsSupport;
+import jetbrains.buildServer.agent.BuildAgentConfiguration;
+import jetbrains.buildServer.agent.BuildProgressLogger;
+
+import org.apache.log4j.Logger;
+
+public class GitVcsOnAgent implements CheckoutOnAgentVcsSupport {
+  Logger log = Logger.getLogger(GitVcsOnAgent.class);
+
+  public void updateSources(BuildProgressLogger logger, File workingDirectory, VcsRoot root, String newVersion, IncludeRule includeRule)
+   throws VcsException
+  {
+    log.info("updateSources: " + workingDirectory + " " + newVersion);
+  }
+
+  public String getName()
+  {
+    return "git";
+  }
+}
+
+
+
