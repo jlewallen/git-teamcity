f709e4476d07e1f638532fe13646d312ac46741e
Jacob Lewallen <jlewallen@gmail.com>
2009-03-22 11:17:36 -0700

    Forcing the SimpleDateFormat to use US locale when parsing.

--- src/main/java/org/hivedb/teamcity/plugin/Constants.java
+++ src/main/java/org/hivedb/teamcity/plugin/Constants.java
@@ -1,12 +1,13 @@
 package org.hivedb.teamcity.plugin;
 
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class Constants {
   
   private static final String GIT_DATE_FORMAT = "EEE MMM dd HH:mm:ss yyyy Z";
   private static final String VERSION_DATE_FORMAT = "MM/dd/yyyy HH:mm:ss";
-  public static final SimpleDateFormat GIT_DATE = new SimpleDateFormat(Constants.GIT_DATE_FORMAT);
-  public static final SimpleDateFormat VERSION_DATE = new SimpleDateFormat(Constants.VERSION_DATE_FORMAT);
+  public static final SimpleDateFormat GIT_DATE = new SimpleDateFormat(Constants.GIT_DATE_FORMAT, Locale.US);
+  public static final SimpleDateFormat VERSION_DATE = new SimpleDateFormat(Constants.VERSION_DATE_FORMAT, Locale.US);
 
 }
